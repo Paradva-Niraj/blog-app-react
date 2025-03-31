6c54324a7d37fcc5ccfbebb777f7ea659f86d256
niraj <nirajparadva2004@gmail.com>
2025-03-31 21:46:45 +0530

    create app write services
    creating post and user services for easy front end.

---
+++ src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import conf from "../../conf/conf";
+import { Client,Account,ID } from "appwrite";
+
+//user login and registration services 
+
+export class AuthSaervice {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteurl)
+        .setProject(conf.appwriteprojectid);
+
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email,password,name }){
+        try{
+            userAccount = await this.account.create(ID.unique(),email,password,name)
+            if(userAccount)
+            {
+                //return userAccount; call another message
+                this.login({email,password});
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch (error){
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logOut(){
+        try{
+            return this.account.deleteSessions();
+        }
+        catch(error)
+        {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthSaervice();
+
+export default authService;
---
+++ src/appwrite/config.js
@@ -0,0 +1,141 @@
+import conf from "../../conf/conf";
+import { Client,ID,Databases,Storage,Query } from "appwrite";
+
+
+//after user login services like handle post
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteurl)
+        .setProject(conf.appwriteprojectid);
+
+        this.databases = new Databases(this.client);
+
+        this.bucket = new Storage(this.client)
+        
+    }
+
+    async createPost({title,slug,content,featuredimage,status,userid}){
+        try {
+            return await this.databases.createDocument(conf.appwritedatabaseid,conf.appwritecollectionid,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredimage,
+                    status,
+                    userid,
+                }
+            )
+        } catch (error) {
+            console.log("app write",error);  
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredimage,status}){
+        try {
+            return await this.databases.updateDocument(conf.appwritedatabaseid,
+                conf.appwritecollectionid,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredimage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("app write",error);
+            
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwritedatabaseid,
+                conf.appwritecollectionid,
+                slug,
+            )
+            return true;
+        } catch (error) {
+            console.log("app write",error);
+            return false;
+        }
+    }
+
+    async viewPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwritedatabaseid,
+                conf.appwritecollectionid,
+                slug,
+            )
+        } catch (error) {
+            console.log("app Write",error);
+            return false
+        }
+    }
+
+    async viewAllPost(){
+        try {
+            // we not use list document because we have active or diactive some post that e just view only active status that why we use getdata with query
+            // return await this.databases.listDocuments(
+            //     conf.appwritedatabaseid,
+            //     conf.appwritecollectionid,
+            //     [
+            //         Query.equal()
+            //     ]
+            // )            
+            return await this.databases.getDocument(
+                conf.appwritedatabaseid,
+                conf.appwritecollectionid,
+                [
+                    Query.equal('status',1)
+                ]
+            )
+        } catch (error) {
+            console.log("app write",error)
+        }
+    }
+
+    async uploadfile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwritebucketid,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("write app",error);
+            return false;
+        }
+    }
+
+    async deletFile(slug){
+        try {
+            return await this,this.bucket.deleteFile(
+                conf.appwritebucketid,
+                slug
+            )
+            return true
+        } catch (error) {
+            console.log(error);
+           return false 
+        }
+    }
+
+    getFilePrev(slug){
+        return this.bucket.getFilePreview(
+            conf.appwritebucketid,
+            slug
+        )
+    }
+}
+
+const service = new Service();
+export default service;
